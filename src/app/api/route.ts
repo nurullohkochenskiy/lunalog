52dacdc7f5cfdaf8a81e271782f775bb31b66d47
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-06-29 12:54:49 +0500

    fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  const req = await request.json();
+let data = {
+  record: {
+    email: "testemail@mail.com",
+  },
+};
+export async function GET(request: Request) {
   return NextResponse.json({
     status: 200,
-    req,
+    data,
+    message: "Data is here",
+  });
+}
+export async function POST(request: Request) {
+  const req = await request.json();
+  data = req;
+  return NextResponse.json({
+    status: 200,
+    data,
     message: "Data has been updated",
   });
 }
